d103c7ef07fcb7564d59e84859e3691593173f8d
Sümeyye Sarıçam <sumeyyesaricam@users.noreply.github.com>
2017-07-02 01:57:31 +0300

    Update Config.java

--- QSearch/app/src/main/java/smyy/qsearch/helper/Config.java
+++ QSearch/app/src/main/java/smyy/qsearch/helper/Config.java
@@ -6,13 +6,13 @@ package smyy.qsearch.helper;
 
 public class Config {
 
-    public static String baseUrl="https://mysterious-temple-11049.herokuapp.com";
-    public static String botUrl="http://dahi.ai";
+    public static String baseUrl="";
+    public static String botUrl="";
     public static final int NOTIFICATION_ID = 100;
     public static final int NOTIFICATION_ID_BIG_IMAGE = 101;
     public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
     public static final String TOPIC_GLOBAL = "global";
     public static final String PUSH_NOTIFICATION = "pushNotification";
     public static final String REGISTRATION_COMPLETE = "registrationComplete";
-    public static final String SHARED_PREF = " qsearch-47e7c";
+    public static final String SHARED_PREF = "";
 }
